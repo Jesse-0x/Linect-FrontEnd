eea7e8e00a4649f78b33a904fc4696e6c98e5aa1
Jesse0x <jgong@whschool.org>
2022-06-23 23:53:48 -0400

    Did nothing XD | Daily Maintenance 0.1.0 build 000114

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,15 @@
 import App from './App'
 
-import HelloWorld from './components/Test/HelloWorld.vue'
+import Test from './components/Test/HelloWorld.vue'
+import Applications from './components/Test/HelloWorld.vue'
+import Requests from './components/Test/HelloWorld.vue'
+import Forums from './components/Test/HelloWorld.vue'
+import Update from './components/Test/HelloWorld.vue'
+import About from './components/Test/HelloWorld.vue'
+import Developers from './components/Test/HelloWorld.vue'
+import Supports from './components/Test/HelloWorld.vue'
+import Login from './components/Test/HelloWorld.vue'
+import Registrations from './components/Test/HelloWorld.vue'
 
 export default [{
     path: '/',
@@ -8,11 +17,47 @@ export default [{
     children:[
         {
             path: '/test',
-            component: HelloWorld
+            component: Test
         },
         {
-            path: '/applications',
-            component: HelloWorld
+            path: '/Applications',
+            component: Applications
+        },
+        {
+            path: '/Requests',
+            component: Requests
+        },
+        {
+            path: '/Forums',
+            component: Forums
+        },
+        {
+            path: '/Update',
+            component: Update
+        },
+        {
+            path: '/About',
+            component: About
+        },
+        {
+            path: '/Developers',
+            component: Developers
+        },
+        {
+            path: '/Supports',
+            component: Supports
+        },
+        {
+            path: '/Login',
+            component: Login
+        },
+        {
+            path: '/Registrations',
+            component: Registrations
+        },
+        {
+            path: '/Business',
+            redirect: '----eliminate----'
         },
     ]
 }]
