5a6bcad54c06f6b8612d83d3e51a13dfa279a5dd
Jesse0x <jgong@whschool.org>
2022-06-29 15:57:44 -0400

    Finishing up main Choices' layout 0.1.0 build 000201

--- src/components/Home/MainChoices.vue
+++ src/components/Home/MainChoices.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="flex-1 bg-gray-400 mt-5 w-full h-20">
-    <div class="main_snap_box from-[#FEB692] to-[#EA5455] ">
-      <span class="">New Arrivals of App & API</span>
+  <div class="flex mt-7 w-full h-20">
+    <div class="main_snap_box from-[#ffdeab] to-[#f0898c] ">
+      <span class="main_snap_box_title">New Arrivals of App & API</span>
     </div>
-    <div class="main_snap_box from-[#FEB692] to-[#EA5455] ">
-      <span class="">New Arrivals of App & API</span>
+    <div class="main_snap_box from-[#ffefa8] to-[#fca272] ">
+      <span class="main_snap_box_title">Fresh Requests of new ideas</span>
     </div>
-    <div class="main_snap_box from-[#FEB692] to-[#EA5455] ">
-      <span class="">New Arrivals of App & API</span>
+    <div class="main_snap_box from-[#5EFCE8] to-[#9591ff] ">
+      <span class="main_snap_box_title">Editors Choices & Suggestions</span>
     </div>
-    <div class="main_snap_box from-[#FEB692] to-[#EA5455] ">
-      <span class="">New Arrivals of App & API</span>
+    <div class="main_snap_box from-[#9cffc0] to-[#6be393] ">
+      <span class="main_snap_box_title">New Updates of App & API</span>
     </div>
   </div>
 </template>
