b5698847d115b01aeac4f653f2197f5ed75a2592
OwO <gongr@go.oes.edu>
2023-02-05 02:55:20 -0800

    YaY using other

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <home-dashboard></home-dashboard>
+</template>
+
+<script>
+import HomeDashboard from "@/components/Dashboard/dashboard";
+
+export default {
+  name: 'App',
+  components: {
+    HomeDashboard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
